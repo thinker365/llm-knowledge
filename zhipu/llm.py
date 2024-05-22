3c713dd70197f0e8b75705f7156e74145c2e3372
liulinyuan <linyuan.liu@heartmed.com.cn>
2024-05-22 17:12:47 +0800

    debug

--- zhipu/llm.py
+++ zhipu/llm.py
@@ -15,61 +15,62 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv('../.env'))
 
-client = ZhipuAI(
-	api_key=os.environ["ZHIPUAI_API_KEY"]
-)
 
 class ZhipuAILLM(LLM, ABC):
-	# 默认选用 glm-4 模型
-	model: str = "glm-4"
-	# 温度系数
-	temperature: float = 0.1
-	# API_Key
-	api_key: str = os.environ["ZHIPUAI_API_KEY"]
-	
-	def _call(self, prompt: str, stop: Optional[List[str]] = None,
-	          run_manager: Optional[CallbackManagerForLLMRun] = None,
-	          **kwargs: Any):
-		def gen_glm_params(prompt):
-			'''
-			构造 GLM 模型请求参数 messages
+    # 默认选用 glm-4 模型
+    model: str = "glm-4"
+    # 温度系数
+    temperature: float = 0.1
+    # API_Key
+    api_key: str = os.environ["ZHIPUAI_API_KEY"]
 
-			请求参数：
-				prompt: 对应的用户提示词
-			'''
-			messages = [{"role": "user", "content": prompt}]
-			return messages
-		
-		client = ZhipuAI(
-			api_key=self.api_key
-		)
-		
-		messages = gen_glm_params(prompt)
-		response = client.chat.completions.create(
-			model=self.model,
-			messages=messages,
-			temperature=self.temperature
-		)
-		
-		if len(response.choices) > 0:
-			return response.choices[0].message.content
-		return "generate answer error"
-	
-	# 首先定义一个返回默认参数的方法
-	@property
-	def _default_params(self) -> Dict[str, Any]:
-		"""获取调用Ennie API的默认参数。"""
-		normal_params = {
-			"temperature": self.temperature,
-		}
-		# print(type(self.model_kwargs))
-		return {**normal_params}
-	
-	@property
-	def _llm_type(self) -> str:
-		return "Zhipu"
-	
-	@property
-	def _identifying_params(self) -> Mapping[str, Any]:
-		"""Get the identifying parameters."""
-		return {**{"model": self.model}, **self._default_params}
+    def _call(self, prompt: str, stop: Optional[List[str]] = None,
+              run_manager: Optional[CallbackManagerForLLMRun] = None,
+              **kwargs: Any):
+        def gen_glm_params(prompt):
+            '''
+            构造 GLM 模型请求参数 messages
+
+            请求参数：
+                prompt: 对应的用户提示词
+            '''
+            messages = [{"role": "user", "content": prompt}]
+            return messages
+
+        client = ZhipuAI(
+            api_key=self.api_key
+        )
+
+        messages = gen_glm_params(prompt)
+        response = client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            temperature=self.temperature
+        )
+
+        if len(response.choices) > 0:
+            return response.choices[0].message.content
+        return "generate answer error"
+
+    # 首先定义一个返回默认参数的方法
+    @property
+    def _default_params(self) -> Dict[str, Any]:
+        """获取调用Ennie API的默认参数。"""
+        normal_params = {
+            "temperature": self.temperature,
+        }
+        # print(type(self.model_kwargs))
+        return {**normal_params}
+
+    @property
+    def _llm_type(self) -> str:
+        return "Zhipu"
+
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        """Get the identifying parameters."""
+        return {**{"model": self.model}, **self._default_params}
+
+
+if __name__ == '__main__':
+    zhipu = ZhipuAILLM()

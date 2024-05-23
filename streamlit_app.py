cc7991c69b5dbf8510c7f8cf53a0224e3e19ae58
liulinyuan <linyuan.liu@heartmed.com.cn>
2024-05-23 16:47:38 +0800

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 
 from zhipu.embedding import ZhipuAIEmbeddings
-# from langchain_community.vectorstores import Chroma
-from langchain.vectorstores.chroma import Chroma
+from langchain_community.vectorstores import Chroma
+# from langchain.vectorstores.chroma import Chroma
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
